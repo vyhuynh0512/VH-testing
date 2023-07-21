6c4f755104c5f1a7c4c63b08cd144ab02c8b482e
vyhuynh0512 <131527943+vyhuynh0512@users.noreply.github.com>
2023-07-21 04:58:32 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React with Vycon
+          GitHub Codespaces <span className="heart">♥️</span> React with Vy
         </p>
         <p className="small">
           Edit <code>src/App.js</code> and save to reload.
